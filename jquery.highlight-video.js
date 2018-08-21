6c14d9b9db89b5f1c66327e073be35502c89d9a4
noah-st-amand <noah@tookish.net>
2018-08-21 19:45:19 -0400

    Fixed highlight video for anonymous users

--- jquery.highlight-video.js
+++ jquery.highlight-video.js
@@ -1,4 +1,9 @@
-(function ($){
+(function($){
+  // see https://stackoverflow.com/questions/17753525/onyoutubeiframeapiready-inside-jquerydocument-ready/24475631#24475631
+  var YTdeferred = $.Deferred();
+  window.onYouTubeIframeAPIReady = function(){
+    YTdeferred.resolve(window.YT);
+  };
   $(document).ready(function(){
     // YouTube iframe API is already loaded (when required) thanks to the pi_video module
     if ($('#block-highlightvideo iframe').length > 0) {
@@ -8,27 +13,30 @@
       var video_html_id_parts = video_html_id.split('-');
       var video_id = video_html_id_parts[video_html_id_parts.length - 1];
       $('#queenslaw-highlight-video iframe').remove();
-      var player = new YT.Player('queenslaw-highlight-video', {
-        videoId: video_id,
-        width: 560,
-        height: 316,
-        playerVars: {
-          playlist: video_id, // otherwise loop doesn't work
-          autoplay: 1,
-          controls: 0,
-          showinfo: 0,
-          modestbranding: 1,
-          loop: 1,
-          fs: 0,
-          cc_load_policty: 0,
-          iv_load_policy: 3,
-          autohide: 0
-        },
-        events: {
-          onReady: function(e) {
-            e.target.mute();
+      var player;
+      YTdeferred.done(function(YT){
+        player = new YT.Player('queenslaw-highlight-video', {
+          videoId: video_id,
+          width: 560,
+          height: 316,
+          playerVars: {
+            playlist: video_id, // otherwise loop doesn't work
+            autoplay: 1,
+            controls: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            loop: 1,
+            fs: 0,
+            cc_load_policty: 0,
+            iv_load_policy: 3,
+            autohide: 0
+          },
+          events: {
+            onReady: function(e) {
+              e.target.mute();
+            }
           }
-        }
+        });
       });
     }
   });
